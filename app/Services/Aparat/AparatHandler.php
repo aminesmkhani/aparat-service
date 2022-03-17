d9a6c7487b5b424c6f7718ce0fb6b91b358724a5
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-03-17 10:08:22 +0330

    add mostViewdVideos!

--- app/Services/Aparat/AparatHandler.php
+++ app/Services/Aparat/AparatHandler.php
@@ -2,7 +2,20 @@
 
 namespace App\Services\Aparat;
 
+use Illuminate\Support\Facades\Http;
+
 class AparatHandler
 {
+    private Http $http;
+    public function __construct(Http $http)
+    {
+        $this->http = $http;
+    }
 
+    public function mostViewedVideos()
+    {
+        $url = 'https://www.aparat.com//etc/api/mostviewedvideos';
+        $response = $this->http::get($url);
+        dd($response->json());
+    }
 }
