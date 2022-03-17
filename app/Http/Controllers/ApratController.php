403d959d3b800637a88dc90f15327010db51f10e
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-03-17 10:11:03 +0330

    Add Aparat Service in Aparat Controller!

---
+++ app/Http/Controllers/ApratController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Aparat\AparatHandler;
+use Illuminate\Http\Request;
+
+class ApratController extends Controller
+{
+    private AparatHandler $aparat;
+    public function __construct(AparatHandler $aparat)
+    {
+        $this->aparat = $aparat;
+    }
+
+    public function index()
+    {
+      $videos = $this->aparat->mostViewedVideos();
+    }
+
+}
